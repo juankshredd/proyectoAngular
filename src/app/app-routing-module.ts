bc22cda188301ad04cf3bb0ff9af522edc7207be
juankshredd <juankmarcelo@gmail.com>
2022-05-07 17:13:04 -0500

    se agregan rutas basicas al home products y about, tambien se redirecciona al home si no hay ruta valida

---
+++ src/app/app-routing-module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { ProductsComponent } from './pages/products/products.component';
+
+const app_routes: Routes = [
+    {path: 'home', component: PortafolioComponent},
+    { path: 'about', component: AboutComponent},
+    {path: 'products', component: ProductsComponent},
+    {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(app_routes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
+//rutas
 import { BrowserModule } from '@angular/platform-browser';
-
+//componentes
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { AppRoutingModule } from './app-routing-module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    ProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
